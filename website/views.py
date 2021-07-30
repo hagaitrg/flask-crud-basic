2ad100339a0f97a4cd8c7ee98bdd06ca4d013be1
hagaitrg <disahagait@gmail.com>
2021-07-30 15:05:40 +0700

    design auth layouts

--- website/views.py
+++ website/views.py
@@ -5,5 +5,6 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @views.route("/home")
 def home():
+    # menggunakan templating jinja 
     return render_template("home.html", name="Joe Biden")
 
