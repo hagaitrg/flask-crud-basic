991fde481ba73fbbb1a45009b54ca81da7af1438
hagaitrg <disahagait@gmail.com>
2021-07-31 16:19:03 +0700

    setup request HTTP and user models

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,19 @@
 from website import views
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for,request
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login",  methods=['GET', 'POST'])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
     return render_template("login.html")
 
-@auth.route("/register")
+@auth.route("/register" , methods=['GET', 'POST'])
 def register():
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
     return render_template("register.html")
 
 @auth.route("/log-out")
