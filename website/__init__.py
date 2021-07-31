c447733efde330f45aab300407eba6786bb66f31
hagaitrg <disahagait@gmail.com>
2021-07-31 16:18:57 +0700

    setup request HTTP and user models

--- website/__init__.py
+++ website/__init__.py
@@ -3,9 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hellomfs"
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # Mengambil semua fungsi dari class views dan auth
     from .views import views
@@ -15,4 +20,14 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    # memanggil models user
+    from .models import User
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database")
